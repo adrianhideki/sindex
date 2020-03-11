544d05702d756fd407acc2f7fff3c21a2039023b
Adrian Hideki <adrian.hideki.br@gmail.com>
2020-03-11 08:04:09 -0300

    Ajustes layouts

--- sindex/frmMain.Designer.cs
+++ sindex/frmMain.Designer.cs
@@ -34,7 +34,12 @@
             this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
             this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
             this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.pnlLogin = new MetroFramework.Controls.MetroPanel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroCheckBox1 = new MetroFramework.Controls.MetroCheckBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
             ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager)).BeginInit();
+            this.pnlLogin.SuspendLayout();
             this.SuspendLayout();
             // 
             // metroStyleManager
@@ -59,7 +64,7 @@
             this.metroTextBox1.CustomButton.Visible = false;
             this.metroTextBox1.Lines = new string[] {
         "metroTextBox1"};
-            this.metroTextBox1.Location = new System.Drawing.Point(23, 113);
+            this.metroTextBox1.Location = new System.Drawing.Point(21, 190);
             this.metroTextBox1.MaxLength = 32767;
             this.metroTextBox1.Name = "metroTextBox1";
             this.metroTextBox1.PasswordChar = '\0';
@@ -78,7 +83,7 @@
             // metroLabel1
             // 
             this.metroLabel1.AutoSize = true;
-            this.metroLabel1.Location = new System.Drawing.Point(23, 75);
+            this.metroLabel1.Location = new System.Drawing.Point(21, 168);
             this.metroLabel1.Name = "metroLabel1";
             this.metroLabel1.Size = new System.Drawing.Size(81, 19);
             this.metroLabel1.TabIndex = 1;
@@ -87,7 +92,7 @@
             // metroLabel2
             // 
             this.metroLabel2.AutoSize = true;
-            this.metroLabel2.Location = new System.Drawing.Point(23, 170);
+            this.metroLabel2.Location = new System.Drawing.Point(21, 226);
             this.metroLabel2.Name = "metroLabel2";
             this.metroLabel2.Size = new System.Drawing.Size(83, 19);
             this.metroLabel2.TabIndex = 2;
@@ -109,7 +114,7 @@
             this.metroTextBox2.CustomButton.Visible = false;
             this.metroTextBox2.Lines = new string[] {
         "metroTextBox2"};
-            this.metroTextBox2.Location = new System.Drawing.Point(23, 218);
+            this.metroTextBox2.Location = new System.Drawing.Point(21, 248);
             this.metroTextBox2.MaxLength = 32767;
             this.metroTextBox2.Name = "metroTextBox2";
             this.metroTextBox2.PasswordChar = '\0';
@@ -125,21 +130,74 @@
             this.metroTextBox2.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
             this.metroTextBox2.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
             // 
+            // pnlLogin
+            // 
+            this.pnlLogin.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pnlLogin.Controls.Add(this.metroButton1);
+            this.pnlLogin.Controls.Add(this.metroCheckBox1);
+            this.pnlLogin.Controls.Add(this.metroLabel3);
+            this.pnlLogin.Controls.Add(this.metroLabel2);
+            this.pnlLogin.Controls.Add(this.metroTextBox2);
+            this.pnlLogin.Controls.Add(this.metroLabel1);
+            this.pnlLogin.Controls.Add(this.metroTextBox1);
+            this.pnlLogin.HorizontalScrollbarBarColor = true;
+            this.pnlLogin.HorizontalScrollbarHighlightOnWheel = false;
+            this.pnlLogin.HorizontalScrollbarSize = 10;
+            this.pnlLogin.Location = new System.Drawing.Point(2, 6);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(300, 477);
+            this.pnlLogin.TabIndex = 4;
+            this.pnlLogin.VerticalScrollbarBarColor = true;
+            this.pnlLogin.VerticalScrollbarHighlightOnWheel = false;
+            this.pnlLogin.VerticalScrollbarSize = 10;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroLabel3.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel3.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel3.Location = new System.Drawing.Point(0, 41);
+            this.metroLabel3.Margin = new System.Windows.Forms.Padding(0);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(300, 87);
+            this.metroLabel3.TabIndex = 5;
+            this.metroLabel3.Text = "sindex";
+            this.metroLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // metroCheckBox1
+            // 
+            this.metroCheckBox1.AutoSize = true;
+            this.metroCheckBox1.Location = new System.Drawing.Point(21, 289);
+            this.metroCheckBox1.Name = "metroCheckBox1";
+            this.metroCheckBox1.Size = new System.Drawing.Size(113, 15);
+            this.metroCheckBox1.TabIndex = 6;
+            this.metroCheckBox1.Text = "metroCheckBox1";
+            this.metroCheckBox1.UseSelectable = true;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(21, 327);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(240, 29);
+            this.metroButton1.TabIndex = 7;
+            this.metroButton1.Text = "metroButton1";
+            this.metroButton1.UseSelectable = true;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 650);
-            this.Controls.Add(this.metroTextBox2);
-            this.Controls.Add(this.metroLabel2);
-            this.Controls.Add(this.metroLabel1);
-            this.Controls.Add(this.metroTextBox1);
+            this.ClientSize = new System.Drawing.Size(800, 485);
+            this.Controls.Add(this.pnlLogin);
+            this.MinimumSize = new System.Drawing.Size(385, 385);
             this.Name = "frmMain";
             this.ShadowType = MetroFramework.Forms.MetroFormShadowType.None;
-            this.Text = "sindex";
             ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager)).EndInit();
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -150,6 +208,10 @@
         private MetroFramework.Controls.MetroTextBox metroTextBox2;
         private MetroFramework.Controls.MetroLabel metroLabel2;
         private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroPanel pnlLogin;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox1;
     }
 }
 
