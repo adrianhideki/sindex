17bd3282338e4c5f72208b6b8bccc93b17de4972
Adrian Hideki <adrian.hideki.br@gmail.com>
2020-09-02 21:40:15 -0300

    removing objects

--- sindex/bin/Debug/scripts/03_functions/fn_GetServerId.sql
+++
@@ -1,8 +0,0 @@
-CREATE FUNCTION dbo.fn_GetServerId()
-RETURNS TABLE
-AS RETURN(
-  SELECT [server].server_id
-  FROM sys.servers
-       INNER JOIN dbo.[server]
-	   ON [server].server_name = servers.name
-);
